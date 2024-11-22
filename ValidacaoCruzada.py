945bf868da36a2bd4c996943f114e91051974c03
adrianofpereira <nanojau@gmail.com>
2024-11-22 16:04:22 -0300

    Tunning dos Parâmetros - Valicação Cruzada

--- ValidacaoCruzada.py
+++ ValidacaoCruzada.py
@@ -13,17 +13,37 @@ from tensorflow.keras import backend as k
 x = pd.read_csv('C:/Users/nanojau/OneDrive/Documentos/CURSOS/DeepLearningWithPythonUdemy/entradas_breast.csv')
 y = pd.read_csv('C:/Users/nanojau/OneDrive/Documentos/CURSOS/DeepLearningWithPythonUdemy/saidas_breast.csv')
 
-def criar_rede():
-    k.clear_session()
-    rede_neural = Sequential([
-        tf.keras.layers.InputLayer(shape=(30,)),
-        tf.keras.layers.Dense(units=16, activation='relu', kernel_initializer='random_uniform'),
-        tf.keras.layers.Dense(units=16, activation='relu', kernel_initializer='random_uniform'),
-         tf.keras.layers.Dense(units=16, activation='sigmoide')])
-    otimizador = tf.keras.optimizer.Adam(learning_rate = 0.001, clipvalue=0.5)
-    rede_neural.compile(optimizer = otimizador, loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
-    return rede_neural
+def criar_rede(optimizer, loss, kernel_initializer, activation, neurons):
+  k.clear_session()
+  rede_neural = Sequential([
+      tf.keras.layers.InputLayer(shape=(30,)),
+      tf.keras.layers.Dense(units=neurons, activation=activation, kernel_initializer=kernel_initializer),
+      tf.keras.layers.Dropout(rate = 0.2),
+      tf.keras.layers.Dense(units=neurons, activation=activation, kernel_initializer=kernel_initializer),
+      tf.keras.layers.Dropout(rate = 0.2),
+      tf.keras.layers.Dense(units=1, activation = 'sigmoid')])
+  rede_neural.compile(optimizer = optimizer, loss = loss, metrics = ['binary_accuracy'])
+  return rede_neural
 
-    rede_neural = KerasClassifier(model = criar_rede, epochs=100, batch_size=10)
+    rede_neural = KerasClassifier(model = criar_rede)
 
-    resultados = cross_val_score(estimator=rede_neural, x = x, y = y, cv = 10, scoring='accuracy')
+    parametros = {
+    'batch_size': [10, 30],
+    'epochs': [50, 100],
+    'model__optimizer': ['adam', 'sgd'],
+    'model__loss': ['binary_crossentropy', 'hinge'],
+    'model__kernel_initializer': ['random_uniform', 'normal'],
+    'model__activation': ['relu', 'tanh'],
+    'model__neurons': [16, 8]
+}
+parametros = {
+    'batch_size': [10, 30],
+    'epochs': [50],
+    'model__optimizer': ['adam'],
+    'model__loss': ['binary_crossentropy'],
+    'model__kernel_initializer': ['random_uniform', 'normal'],
+    'model__activation': ['relu'],
+    'model__neurons': [16]
+}
+
+parametros
